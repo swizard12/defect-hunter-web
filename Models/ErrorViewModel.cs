a2975726c313adfdeaf2e7df3e4f0028f126c722
Sam Wizard <swizard12@hotmail.co.uk>
2018-05-22 08:16:01 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace defect_hunter_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
